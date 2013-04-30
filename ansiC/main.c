e5646fbe83d0610a64dcb4ed8fa156e7cedaeccf
theBigBadWolf <thebigbadwolf@nowhere.com>
2013-04-30 23:26:59 +0100

    initial commit

---
+++ ansiC/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  ansiC
+//
+//  Created by gabriel on 30/04/2013.
+//  Copyright (c) 2013 theBigBadWolf. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
